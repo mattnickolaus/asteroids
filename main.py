33b32376f091aa78e74cad456df8c8f606dbfdb4
mattnickolaus <mattnickolaus@gmail.com>
2024-10-10 23:04:02 -0400

    Adds player class and draws to screen

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    # Creates Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
         # limits the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
