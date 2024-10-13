fffefd8df5d2909d780b6979714cab2a2a5af7d4
mattnickolaus <mattnickolaus@gmail.com>
2024-10-13 01:54:12 -0400

    Added Player health system and icons

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from asteroidfield import AsteroidField
 from button import Button
 from constants import *
 from player import *
+from player_health_icon import *
 
+
+score = 0
+player_health = 3
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('Asteroids')
     clock = pygame.time.Clock()
-
-
 
 
 
@@ -31,6 +33,7 @@ def main():
         Asteroid.containers = (asteroids, updatable, drawable)
         Shot.containers = (shots, updatable, drawable)
         AsteroidField.containers = updatable
+        PlayerHealthBar.containers = drawable
 
         # Creates Player object (instance has to be after groups are created)
         player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -38,6 +41,19 @@ def main():
         # Create new AsteroidField object
         asteroid_field = AsteroidField()
 
+        health_icon1 = PlayerHealthBar(30, 80)
+        health_icon2 = PlayerHealthBar(60, 80)
+        health_icon3 = PlayerHealthBar(90, 80)
+        global player_health
+
+        # Logic to make the icons disappear
+        if player_health <= 2:
+            health_icon3.kill()
+        if player_health <= 1:
+            health_icon2.kill()
+
+
+        global score
 
         # Delta-time
         dt = 0
@@ -47,24 +63,33 @@ def main():
                 if event.type == pygame.QUIT:
                     exit()
 
+
             for obj in updatable:
                 obj.update(dt)
 
             for asteroid in asteroids:
                 if asteroid.check_collision(player):
-                    print("Game over!")
                     player.kill()
                     asteroid_field.kill()
                     asteroid.kill()
+                    player_health -= 1
                     run = False
 
 
                 for bullet in shots:
                     if bullet.check_collision(asteroid):
                         asteroid.split()
+                        score += 100
                         bullet.kill()
 
             screen.fill("black")
+
+            if player_health == 0:
+                health_icon1.kill()
+
+            # Create Score Render
+            score_img = FONT.render(f"Score: {score}", True, "white")
+            screen.blit(score_img, (20, 20))
 
             for obj in drawable:
                 obj.draw(screen)
@@ -76,7 +101,13 @@ def main():
             dt = clock.tick(60) / 1000
 
 
+        if player_health > 0:
+            game_loop()
+
+
     game_loop()
+
+
 
     restart = Button((SCREEN_WIDTH / 2) - 90, (SCREEN_HEIGHT / 2)- 35, "Restart")
 
@@ -84,6 +115,10 @@ def main():
     while game_over:
 
         if restart.draw_button(screen):
+            global player_health
+            player_health = 3
+            global score
+            score = 0
             game_loop()
 
         text_game_over = GAME_OVER_FONT.render("Game Over!", True, "white")
